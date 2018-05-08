83dc4fe2404f596c5c8bcefa1187bea79fec1754
AshidaSRS <ashida.shin@gmail.com>
2018-05-08 16:08:28 +0200

    Finished

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -1,6 +1,5 @@
 import scala.util.Random
 import breeze.linalg._
-import breeze.math._
 import breeze.numerics._
 
 object MainApp extends App {
@@ -13,24 +12,16 @@ object MainApp extends App {
 
   val trainingSetInputs = DenseMatrix(set1, set2, set3, set4)
   val trainingSetOutputs = DenseMatrix(outSet1).t
-  println(trainingSetInputs)
-  println(trainingSetOutputs)
-  println("-----------")
 
   def serial(): Unit = Random.setSeed(1)
 
   def getRandom = 2 * Random.nextDouble() -1
   var weights: DenseMatrix[Double] = DenseMatrix.create(3, 1, Array(getRandom, getRandom, getRandom))
-  println(weights)
-  println("-----------")
-  def think(inputs: DenseMatrix[Int]): DenseMatrix[Int] = convert(sigmoid(convert(inputs, Double) * weights), Int)
+  println(s"First weigths \n$weights")
 
-  def sigmoidDerivative(x: DenseMatrix[Int]): DenseMatrix[Int] = {
-    println(x)
-    val a = x * (1 -:- x)
-    println(a)
-    a
-  }
+  def think(inputs: DenseMatrix[Int]): DenseMatrix[Double] = sigmoid(convert(inputs, Double) * weights)
+
+  def sigmoidDerivative(x: DenseMatrix[Double]): DenseMatrix[Double] = sigmoid(x) *:* (-sigmoid(x) + 1.0)
 
   def train(inputs: DenseMatrix[Int], outputs: DenseMatrix[Int]): Unit = {
 
@@ -38,18 +29,21 @@ object MainApp extends App {
       n match {
         case 0 => weights
         case _ =>
-          val output: DenseMatrix[Int] = think(inputs)
-          val error: DenseMatrix[Double] = convert(outputs, Double) - convert(output, Double)
-          val adjust: DenseMatrix[Double] = convert(inputs, Double).t * (error * convert(sigmoidDerivative(output), Double))
-          weights += adjust
+          val output: DenseMatrix[Double] = think(inputs)
+          val error: DenseMatrix[Double] = convert(outputs, Double) - output
+          val adjust: DenseMatrix[Double] = convert(inputs, Double).t * (error *:* sigmoidDerivative(output))
+          weights = weights + adjust
           trainAux(inputs, outputs, n - 1)
       }
     }
 
     trainAux(inputs, outputs, 10000)
   }
-  println("-----------")
   train(trainingSetInputs, trainingSetOutputs)
+
+  println("Last weights")
   println(weights)
+
+  println(s"New situation [1,0,0]: ")
   println(think(DenseMatrix.create(1,3, Array(1,0,0))))
 }
